7ed72a10f3c2af40c5999b5ac938c73200dedf1c
steve1442 <keller1442@gmail.com>
2020-04-14 21:51:02 -0400

    implemented first command

---
+++ computerVision.js
@@ -0,0 +1,78 @@
+const cv = require('opencv4nodejs');
+//const jimp = require('jimp');
+const ImgDownload = require('image-downloader');
+const fs = require('fs');
+
+const classifiers = {
+    FACE: "face",
+    EYE: "eye",
+    BODY: "body"
+}
+
+async function downloadImage(url,path){
+    const options = {
+        url: url,
+        dest: path
+    }
+    filename = ''
+    try{
+        filename = (await ImgDownload.image(options)).filename;
+            console.log('Saved to', filename)  // Saved to /path/to/dest/photo.jpg
+            return filename;
+    }
+    catch(e){
+        console.error(e);
+        return 1;
+    }
+}
+
+function detect(path, classifier){
+    switch(classifier){
+        case "face":
+            classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_DEFAULT);
+            break;
+        case "eye":
+            classifier = new cv.CascadeClassifier(cv.HAAR_EYE);
+            break;
+        case "body":
+            classifier = new cv.CascadeClassifier(cv.HAAR_FULLBODY);
+        default:
+            classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_DEFAULT);
+            break;
+    }
+    const mat = cv.imread(path);
+    const matGray = mat.bgrToGray();
+    return (classifier.detectMultiScale(matGray)).objects;
+}
+
+function drawDick(mat, point,size){
+    const ballSize = size * 0.2;
+    const center = point;
+    let leftBall = new cv.Point(center.x - ballSize, center.y +  size * 0.3);
+    let rightBall = new cv.Point(center.x + ballSize, center.y + size * 0.3);
+    let shaftPos = new cv.Point(center.x,center.y - size * 0.1);
+    mat.drawCircle(leftBall, ballSize, new cv.Vec(0, 255, 0), 5);
+    mat.drawCircle(rightBall, ballSize, new cv.Vec(0, 255, 0), 5);
+    let shaft = new cv.RotatedRect(shaftPos,new cv.Size(ballSize,size * 0.9), 0);
+    mat.drawEllipse(shaft, new cv.Vec(0, 255, 0), 5);
+    return mat
+}
+
+function dickhead(path){
+    let points = detect(path, classifiers.FACE);
+    const mat = cv.imread(path);
+    if(points == 0){
+        drawDick(mat, new cv.Point(mat.width/2.0,mat.height/2.0), mat.height);
+    }
+    else{
+        for(let i = 0; i < points.length; i++){
+            drawDick(mat, new cv.Point(points[i].x + points[i].width/2.0,points[i].y + points[i].height/2.0), points[i].height);
+        }
+    }
+    cv.imwrite(path,mat);
+}
+
+module.exports = {
+    downloadImage:downloadImage,
+    dickhead: dickhead
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const computerVision = require('./computerVision');
+const fs = require('fs');
+const Discord = require('discord.js');
+const path = require('path');
+const client = new Discord.Client();
+let image = 0;
+const filesTypes = [".png",".jpeg",".jpg"];
+
+
+client.on('ready', () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("!stevehelp"); 
+    if(!fs.existsSync('temp/')){
+        fs.mkdirSync('temp/');
+    }
+});
+
+client.on('message', msg =>{
+    if(msg.content.startsWith('!dick')){
+        msg.attachments.forEach(async a =>{
+            let filePath = await computerVision.downloadImage(a.url,'temp/');
+            console.log(filePath);
+            const stats = fs.statSync(filePath);
+            if(stats["size"] > 8000000){
+                msg.channel.send('too big sorry');
+                return 1;
+            }
+            const filetype = path.extname(filePath).toLowerCase();
+            let flag = false;
+            filesTypes.forEach(type =>{
+                if(filetype == type){
+                    flag = true;
+                }
+            });
+            if(!flag){
+                msg.channel.send("cant accept type" + filetype);
+                return 1;
+            }
+            computerVision.dickhead(filePath);
+            const attachment = new Discord.MessageAttachment(filePath);
+            await msg.channel.send(attachment);
+            fs.unlink(filePath, () =>{
+                console.log("succesfully deleted: " + filePath);
+            });
+            image++;
+        });
+        return 0;
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
